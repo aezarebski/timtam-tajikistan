0a4a6729ef9bfda92eb7742088ab8bbfd1324cf7
aezarebski <aezarebski@gmail.com>
2025-02-12 16:53:49 +0000

    update the subsampling plotting script to use prevalence not hidden lineages

--- R/postprocessing-4-subsampling.R
+++ R/postprocessing-4-subsampling.R
@@ -9,7 +9,12 @@ library(svglite) # needed for SVG output
 library(xml2)
 ## library(lubridate)
 library(purrr)
+library(furrr)
 library(timtamslamR)
+
+
+warning("DO NOT WORRY about a warning that there are random numbers without a seed.")
+future::plan(future::multisession, workers = parallel::detectCores() - 2)
 
 ## ============================================================
 ## Hard-code visualisation parameters
@@ -23,13 +28,13 @@ legend_background_style <-
   element_rect(colour = "#363636", linewidth = 0.25)
 ## ============================================================
 
-read_hs_values <- function(beast_log, history_times) {
-  tmp <- beast_log |>
-    read_beast2_log() |>
-    select(matches("HistorySize")) |>
-    mutate(log_file = beast_log) |>
-    melt(id.vars = "log_file", variable.name = "parameter", value.name = "value") |>
-    group_by(log_file, parameter) |>
+prev_summary_values <- function(prev_df, model_xml, history_times) {
+  tmp <-
+    prev_df |>
+    dplyr::select(dplyr::matches("TTPrevalence")) |>
+    mutate(xml_file = model_xml) |>
+    melt(id.vars = "xml_file", variable.name = "prevalence", value.name = "value") |>
+    group_by(xml_file, prevalence) |>
     summarise(median = median(value),
               lower = bayestestR::hdi(value, ci = 0.95, verbose = FALSE)[["CI_low"]],
               upper = bayestestR::hdi(value, ci = 0.95, verbose = FALSE)[["CI_high"]],
@@ -47,13 +52,63 @@ read_hs_values <- function(beast_log, history_times) {
 }
 
 ## ============================================================
+config <- as_list(read_xml("config.xml"))
+input_present_rds <- config$files$results$intermediate$present[[1]]
+rm(config)
 
-beast_xml_orig <- "out/subsampling-experiment/xml/timtam-2023-10-15-original.xml"
 output_png <- "out/subsampling-experiment/summary-plot-historysizes.png"
 
+beast_xmls <- c(
+  "out/subsampling-experiment/xml/timtam-2023-10-15-original.xml",
+  "out/subsampling-experiment/xml/timtam-2023-10-15-subsample-0p33.xml",
+  "out/subsampling-experiment/xml/timtam-2023-10-15-subsample-0p66.xml"
+  )
 beast_logs <- c("out/timtam-2023-10-15-original.log",
                 "out/timtam-2023-10-15-subsample-0p66.log",
                 "out/timtam-2023-10-15-subsample-0p33.log")
+beast_trees <-
+  gsub(beast_logs,
+       pattern = ".log",
+       replacement = "-timed-sequences.trees")
+
+## ============================================================
+
+present_val <- readRDS(input_present_rds)
+
+## We will subset the samples to a managable amount by removing the
+## first 10% as burn-in, and then sampling N uniformly selected points
+## from the remaining samples.
+##
+## Here *N = 20* (see `subset_ixs')
+##
+num_to_burn <- 10001
+subset_ixs <- floor(seq.int(from = 1, to = 90000, length.out = 500))
+
+beast_models <-
+  purrr::map(
+           .x = beast_xmls,
+           .f = \(x) timtamslamR::read_beast2_xml(x, present_val, "days")
+         )
+post_dfs <-
+  furrr::future_map(
+           .x = beast_logs,
+           .f = \(x) timtamslamR::read_beast2_log(x, burn = num_to_burn)[subset_ixs,]
+         )
+tree_sets <-
+  furrr::future_map(
+           .x = beast_trees,
+           .f = \(x) timtamslamR::read_beast2_trees(x, burn = num_to_burn)[subset_ixs]
+         )
+prev_dfs <-
+  furrr::future_map(
+           .x = seq_along(beast_xmls),
+           .f = \(ix) timtamslamR::total_prevalence_log(
+                                     post_dfs[[ix]],
+                                     tree_sets[[ix]],
+                                     beast_models[[ix]]
+                                   )
+         )
+
 
 ## ============================================================
 orig_xml <- "out/subsampling-experiment/xml/timtam-2023-10-15-original.xml"
@@ -67,7 +122,7 @@ r0_change_bkwd_times <-
   purrr::pluck(1) |>
   as.numeric()
 
-
+beast_xml_orig <- "out/subsampling-experiment/xml/timtam-2023-10-15-original.xml"
 stopifnot(file.exists(beast_xml_orig))
 beast_model <- xml2::as_list(xml2::read_xml(beast_xml_orig))
 history_times_str <-
@@ -84,11 +139,19 @@ log_labels <- c(
   "out/timtam-2023-10-15-subsample-0p33.log" = "Subsample 33%"
 )
 
+beast_xml_labels <- c(
+  "out/subsampling-experiment/xml/timtam-2023-10-15-original.xml" = "Original",
+  "out/subsampling-experiment/xml/timtam-2023-10-15-subsample-0p33.xml" = "Subsample 66%",
+  "out/subsampling-experiment/xml/timtam-2023-10-15-subsample-0p66.xml" = "Subsample 33%"
+  )
 
-hs_post_df <- beast_logs |>
-  map(read_hs_values, history_times = history_times_num) |>
+prev_post_df <-
+  furrr::future_map2(
+           .x = prev_dfs,
+           .y = beast_xmls,
+           .f = \(x, y) prev_summary_values(x, y, history_times = history_times_num)) |>
   bind_rows() |>
-  mutate(log_file = factor(log_file, levels = beast_logs))
+  mutate(xml_file = factor(xml_file, levels = beast_xmls))
 
 ## ============================================================
 
@@ -97,14 +160,14 @@ hs_post_df <- beast_logs |>
 ## problem.
 warning("DO NOT WORRY about a warning that scale_y_log10() produced infinite values.")
 
-hs_gg <-
-  ggplot(data = hs_post_df,
+prev_gg <-
+  ggplot(data = prev_post_df,
                   aes(x = history_times,
                       y = median,
                       ymin = lower,
                       ymax = upper,
-                      shape = log_file,
-                      colour = log_file)) +
+                      shape = xml_file,
+                      colour = xml_file)) +
   geom_vline(xintercept = r0_change_bkwd_times) +
   geom_pointrange(position = position_dodge(width = 10)) +
   scale_x_reverse() +
@@ -119,17 +182,17 @@ hs_gg <-
                      labels = log_labels,
                      values = scale_shape_vals) +
   labs(x = "Backwards time (days)",
-       y = "Hidden lineages") +
+       y = "Prevalence") +
   theme_bw() +
   theme(legend.position = "none")
 
 
 if (interactive()) {
-  print(hs_gg)
+  print(prev_gg)
 } else {
   plot_width <- 12
   plot_height <- 9
-  ggsave(output_png, hs_gg, width = plot_width, height = plot_height, units = "cm")
-  ggsave(filename = sub("png$", "svg", output_png), plot = hs_gg, width = plot_width, height = plot_height, units = "cm")
-  saveRDS(hs_gg, sub("png$", "rds", output_png))
+  ggsave(output_png, prev_gg, width = plot_width, height = plot_height, units = "cm")
+  ggsave(filename = sub("png$", "svg", output_png), plot = prev_gg, width = plot_width, height = plot_height, units = "cm")
+  saveRDS(prev_gg, sub("png$", "rds", output_png))
 }
